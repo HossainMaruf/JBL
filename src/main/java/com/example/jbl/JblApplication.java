77e1df753ca6b09726da9821c0c708951bf446c3
HossainMaruf <maruficepustian@gmail.com>
2025-12-17 16:01:47 +0600

    Initial

---
+++ src/main/java/com/example/jbl/JblApplication.java
@@ -0,0 +1,13 @@
+package com.example.jbl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JblApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JblApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/jbl/model/Course.java
@@ -0,0 +1,99 @@
+package com.example.jbl.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+// import jakarta.persistence.Table;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+
+
+enum CourseType {THEORY, LAB}
+
+@Entity
+// @Table(name = "courses")
+public class Course {
+    @Id
+    private String code;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private Float credit;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CourseType type;
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    // Constructor
+    public Course() {}
+    public Course(String code, String title, Float credit, CourseType type) {
+        this.code = code;
+        this.title = title;
+        this.credit = credit;
+        this.type = type;
+    }
+
+    // Getters
+    public String getCode() {
+        return code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Float getCredit() {
+        return credit;
+    }
+
+    public CourseType getType() {
+        return type;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    // Setters
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCredit(Float credit) {
+        this.credit = credit;
+    }
+
+    public void setType(CourseType type) {
+        this.type = type;
+    }
+
+    // public void setCreatedAt(LocalDateTime createdAt) {
+    //     this.createdAt = createdAt;
+    // }
+
+    // public void setUpdatedAt(LocalDateTime updatedAt) {
+    //     this.updatedAt = updatedAt;
+    // }
+}
